ffb6b1464af5166516ff24dc27949798b1f44465
Muchachos <indijanero@gmail.com>
2018-04-03 17:59:51 +0200

    ~

---
+++ FileEngine/FileManager.cs
@@ -0,0 +1,25 @@
+﻿using FileEngine.Parsers;
+using System.Collections.Generic;
+
+namespace FileEngine
+{
+    public class FileManager
+    {
+        private IParser _parser;
+
+        public FileManager(IParser parser)
+        {
+            _parser = parser;
+        }
+
+        public void SetParser(IParser parser)
+        {
+            _parser = parser;
+        }
+
+        public IEnumerable<T> Parse<T>(string filepath) where T : class, new()
+        {
+            return _parser.Parse<T>(filepath);
+        }
+    }
+}
